fd03f54dc7bf6d958da20f07aa9736578208a486
Naoki Tani <naoki.1121.hit.and.run48@gmail.com>
2025-07-04 07:29:32 +0900

    <add> 最終調整

--- frontend/src/pages/Log.tsx
+++ frontend/src/pages/Log.tsx
@@ -27,7 +27,7 @@ const Log: React.FC = () => {
       try {
         const formData = new FormData();
         formData.append('user_id', user_id || '');
-        const res = await fetch('http://127.0.0.1:8000/analysis_results/log/test', {
+        const res = await fetch('http://127.0.0.1:8000/log/test', {
           method: 'POST',
           body: formData,
         });
