b312ad84966edea7f30372c7ce7b595c5d3ec430
nyoki610 <yuya.nyoki@gmail.com>
2025-07-03 11:37:50 +0900

    <change> db_router.pyを整形

--- backend/db/db_router.py
+++ backend/db/db_router.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .db import get_dbsession
+from typing import List
 
 # schemas
 from .schemas.analysis_results import AnalysisResult as AnalysisResultSchema
@@ -28,16 +29,16 @@ async def post_analysis_results(
     return await post_analysis_result(db, request)
 
 
+@router.get("/get_by_user_id")
+async def get_analysis_results_by_user_id(
+    user_id: str,
+    db: AsyncSession = Depends(get_dbsession),
+) -> List[AnalysisResultSchema]:
+    return await get_analysis_results_by_user_id_crud(db, user_id)
+
+
 @router.delete("/delete_all_for_development")
 async def delete_all_analysis_results(
     db: AsyncSession = Depends(get_dbsession),
 ):
     return await delete_analysis_results_table_for_development(db)
-
-
-@router.get("/get_by_user_id")
-async def get_analysis_results_by_user_id(
-    user_id: str,
-    db: AsyncSession = Depends(get_dbsession),
-):
-    return await get_analysis_results_by_user_id_crud(db, user_id)
